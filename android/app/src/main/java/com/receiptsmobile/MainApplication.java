e749e1c414273b0ab465ae65c2a863a6dbd2cb22
Leonti Bielski <lbielski@testobject.com>
2016-07-14 19:17:43 +1000

    Upgrade to 0.29.1

---
+++ android/app/src/main/java/com/receiptsmobile/MainApplication.java
@@ -0,0 +1,54 @@
+package com.receiptsmobile;
+
+import android.app.Application;
+import android.util.Log;
+
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.receiptsmobile.files.NetworkFilesPackage;
+import com.receiptsmobile.picker.ImagePickerPackage;
+import com.receiptsmobile.uploader.UploaderPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+              new MainReactPackage(),
+              new VectorIconsPackage(),
+              new ImagePickerPackage(),
+              new NetworkFilesPackage(),
+              new UploaderPackage(),
+              new RNGoogleSigninPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+      return mReactNativeHost;
+  }
+}
