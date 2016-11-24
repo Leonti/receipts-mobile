60e34475ee41533dd82ed1925f0141609494bedc
Leonti Bielski <lbielski@testobject.com>
2016-11-24 19:56:07 +1100

    Fix view not being full-screen on login page

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -22,7 +22,9 @@ class LoginPage extends React.Component {
 
   render() {
       return (
-          <View>
+          <View style={{
+              flex: 1,
+          }}>
             <ToolbarAndroid
               style={styles.toolbar}
               title="Login"
